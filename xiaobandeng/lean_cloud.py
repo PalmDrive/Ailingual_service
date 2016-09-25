db25de7d81f40982df7986fa81974738f9f36c43
Yong <yonglin88@gmail.com>
2016-09-24 19:17:01 -0700

    add media class

--- xiaobandeng/lean_cloud.py
+++ xiaobandeng/lean_cloud.py
@@ -4,6 +4,7 @@ import datetime
 APP_ID = 'hwB46P8KvcGH258ka0JfnMww-gzGzoHsz'
 MASTER_KEY = 'ywhqmYxpFKTyemJrFl8YYT2j'
 CLASS_NAME_TRANSCRIPT = 'Transcript'
+CLASS_NAME_MEDIA = 'Media'
 
 class LeanCloud(object):
     def __init__(self):
@@ -27,9 +28,22 @@ class LeanCloud(object):
         fragment.set('media_src', media_src)
         self.fragments.append(fragment)
 
+    def add_media(self, media_name, media_id, media_url, duration):
+        media = self.Media()
+        media.set('media_id', media_id)
+        media.set('media_name', media_name)
+        media.set('media_url', media_url)
+        media.set('duration', duration)
+        media.set('company_name', duration)
+        media.set('received_at', duration)
+        media.set('transcribed_at', datetime.datetime())
+        media.set('status', 'Auto Transcribed')
+        self.media.append(media)
+
     def upload(self):
         try:
             self.Fragment.save_all(self.fragments)
+            self.Media.save_all(self.media)
         except leancloud.LeanCloudError as e:
             print e
             raise
