0e0b09c8f73a0ed4b5754c3bc7aeffcb8389f85e
jml <ljm@a.cc>
2016-09-26 16:30:39 +0800

    配置变量加入到 config 文件中、

--- xiaobandeng/oss.py
+++ xiaobandeng/oss.py
@@ -1,20 +1,21 @@
 import oss2
 import os
+from env_config import  CONFIG
 
-Access_Key_ID = 'ZblgTWN34znE5dOO'
-Access_Key_Secret = 'S09puSorYwkbdyyAjWpqudXQ8mLvl9'
-
-EndpointHost = 'oss-cn-hangzhou.aliyuncs.com'
-EndpointHead = 'http://'
-BucketName = 'xiaobandeng-staging'
-import datetime
+# OSS_Access_Key_ID = 'ZblgTWN34znE5dOO'
+# OSS_Access_Key_Secret = 'S09puSorYwkbdyyAjWpqudXQ8mLvl9'
+#
+# OSS_EndpointHost = 'oss-cn-hangzhou.aliyuncs.com'
+# OSS_EndpointHead = 'http://'
+# OSS_BucketName = 'xiaobandeng-staging'
 
 
 # This method should be executed asynchronously
 def upload(media_id, file_list):
 
-    auth = oss2.Auth(Access_Key_ID, Access_Key_Secret)
-    bucket = oss2.Bucket(auth, EndpointHead + EndpointHost, BucketName)
+    auth = oss2.Auth(CONFIG.OSS_Access_Key_ID, CONFIG.OSS_Access_Key_Secret)
+    bucket = oss2.Bucket(auth, CONFIG.OSS_EndpointHead + CONFIG.OSS_EndpointHost,
+                         CONFIG.OSS_BucketName)
 
     key = 'media_fragments/%s/' % media_id
 
@@ -26,4 +27,8 @@ def upload(media_id, file_list):
         # print "%s ----end@----%s"%(filename,datetime.datetime.now())
 
 def media_fragment_url(media_id, file_name):
-    return "%s%s.%s/media_fragments/%s/%s" % (EndpointHead, BucketName, EndpointHost, media_id, os.path.basename(file_name))
+    return "%s%s.%s/media_fragments/%s/%s" % (CONFIG.OSS_EndpointHead,
+                                              CONFIG.OSS_BucketName,
+                                              CONFIG.OSS_EndpointHost,
+                                              media_id,
+                                              os.path.basename(file_name))
