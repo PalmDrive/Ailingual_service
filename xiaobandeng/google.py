bfc99078cac3b6ed23a600a8e37b42a5be416b63
Yong <yonglin88@gmail.com>
2016-09-28 18:40:29 -0700

    implement google task

--- xiaobandeng/google.py
+++ xiaobandeng/google.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 import argparse
 import base64
 import json
-from multiprocessing import Pool
+from multiprocessing import mp
 
 from googleapiclient import discovery
 import httplib2
@@ -28,9 +28,12 @@ class GoogleASR(object):
         self.service = discovery.build(
             'speech', 'v1beta1', http=http, discoveryServiceUrl=DISCOVERY_URL)
 
+        self.lanCodes = {'zh' : 'cmn-Hans-CN', 'en' : 'en-US'}
 
     def vop(self, filename, lan='zh'):
         print 'start google vop - ', filename
+
+        lanCode = self.lanCodes[lan]
 
         with open(filename, 'rb') as speech:
             # Base64 encode the binary audio file for inclusion in the JSON
@@ -45,7 +48,7 @@ class GoogleASR(object):
                     'encoding': 'LINEAR16',  # raw 16-bit signed LE samples
                     'sampleRate': 16000,  # 16 khz
                     # See https://goo.gl/A9KJ1A for a list of supported languages.
-                    'languageCode': 'cmn-Hans-CN',  # a BCP-47 language tag
+                    'languageCode': lanCode,  # a BCP-47 language tag
                 },
                 'audio': {
                     'content': speech_content.decode('UTF-8')
@@ -54,72 +57,49 @@ class GoogleASR(object):
         response = service_request.execute()
         return response
 
+    def batch_vop(self, file_list, callback, starts, lan):
+        num_workers = mp.cpu_count()
+        pool = mp.Pool(num_workers)
+        task_list = TaskGroup(file_list, lan, callback, starts)
+        task_list.set_task_type(TaskBaidu, self.access_token)
+        task_list.start()
+        print datetime.datetime.now()
+
 class TaskGoogle(Task):
 
     lans = {"zh": ['zh', 'zh', 'zh', 'en', 'en', 'en'],
             "en": ['en', 'en', 'en', 'zh', 'zh', 'zh']
             }
 
-    def __init__(self, tid, file_name, lan='zh', service):
+    def __init__(self, tid, file_name, pool, lan='zh', service=None):
         super(TaskGoogle, self).__init__(tid, file_name)
         self.service = service
         self.max_try = 6
         self._try = 0
         self.lan = lan
         self.url = self.get_url(lan)
+        self.pool = pool
 
     def start(self):
         print 'start google vop on %s' % self.file_name
-        self.fetch(self.url)
-
-    def fetch(self, url):
-        self.client.fetch(self.get_request(url), self.callback)
-
-    def get_request(self, url):
-        http_header = {'Content-Type': 'audio/wav; rate=%d' % self.rate,
-                       'Content-Length': str(len(self.body)),
-                       }
-
-        return tornado.httpclient.HTTPRequest(url=url, method='POST',
-                                              connect_timeout=120,
-                                              request_timeout=600,
-                                              headers=http_header,
-                                              body=self.body
-                                              )
-
-    def get_url(self, lan):
-        return self.vop_url + '?cuid=' + '123442123233213' + '&token=' + \
-               self.token + '&lan=' + lan
+        res = self.pool.apply_async(self.service.vop, (self.file_name, self.lan))
+        self.callback(res.get())
 
     def retry(self):
         self._try += 1
         if self._try < self.max_try:
             self.fetch(self.get_url(self.lans[self.lan][self._try]))
-            print 'retry %s...%s' % (self.id, datetime.datetime.now())
+            print 'retry %s' % (self.id)
         else:
             self.complete()
             self.result = ''
 
     def callback(self, res):
-        if res.error:
-            print '%s error:%s' % (self.id, res.error)
-            self.retry()
-            return
-
-        res = json.loads(res.body)
-
-        if int(res['err_no']) in (3301, 3302):
-            # print '%s baidu api error :%s'%(self.id,res['err_no'])
-            self.retry()
-            return
-
-        if int(res['err_no']) == 0:
-            self.result = res["result"][0]
-            self.complete()
-            # print '%s====>%s'%(self.id,self.result)
-            return
-
-        self.result = 'Baidu API error: %d %s' % (res['err_no'], res['err_msg'])
+        if len(res['results']) > 0:
+            self.result = res['results'][0]['alternatives'][0]['transcript']
+        else:
+            # handle error
+            self.result = 'Google API error: %s' % (json.dump(res))
         self.complete()
 
 def main(speech_file):
