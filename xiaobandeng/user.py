5a315b78eb9681258316f6c5e5865a191b67f5f4
jml <ljm@a.cc>
2016-10-10 20:17:14 +0800

    authenticate module for pipeline-service.

---
+++ xiaobandeng/user.py
@@ -0,0 +1,65 @@
+# coding:utf-8
+
+import leancloud
+from datetime import datetime
+from env_config import CONFIG
+import traceback, sys
+
+# singleton instance
+class UserMgr(object):
+    _instance = None
+
+    def __new__(cls):
+        if UserMgr._instance:
+            return UserMgr._instance
+        else:
+            instance = object.__new__(cls)
+            UserMgr._instance = instance
+            return UserMgr._instance
+
+    def __init__(self):
+
+        APP_ID = CONFIG.LEANCLOUD_APP_ID
+        MASTER_KEY = CONFIG.LEANCLOUD_MASTER_KEY
+        leancloud.init(APP_ID, MASTER_KEY)
+        self.User = leancloud.User
+
+    def create_user(self, username, passwd):
+        user = self.User()
+        user.set_username(username)
+        user.set_password(passwd)
+
+        try:
+            user.sign_up()
+            res = (True, user)
+        except leancloud.LeanCloudError as e:
+            res = (False, e)
+
+        return res
+
+    def login(self, username, passwd):
+        try:
+            self.User().login(username, passwd)
+            res = True, self.User.get_current()
+        except leancloud.LeanCloudError as e:
+            res = False, e
+
+        return res
+
+
+if __name__ == "__main__":
+    import os, json, env_config
+
+    pwd = os.path.dirname(__file__)
+    env = 'develop'
+    config_file = os.path.join(pwd, "config", env + ".json")
+    config_dict = json.load(open(config_file))
+    env_config.init_config(config_dict)
+
+    user_mgr = UserMgr()
+#    print user_mgr.create_user('hello1', 'world')
+
+    print user_mgr.login('hello','world')
+    print user_mgr.User.get_current()
+
+    print 'over.'
