9189e7a4956d67a1defffc0978bd905a1b56f2f9
jml <ljm@a.cc>
2016-10-12 20:28:49 +0800

    lrc handler .

--- xiaobandeng/server.py
+++ xiaobandeng/server.py
@@ -310,11 +310,59 @@ class SrtHandler(BaseHandler):
             self.write("not exist")
 
 
+class LrcHandler(BaseHandler):
+
+    def get(self, media_id):
+        source = self.get_argument("service_source", 0)
+
+        lc_content_keys = ["content_baidu", "content_google"]
+        self.content_key = lc_content_keys[int(source)]
+
+        lc = lean_cloud.LeanCloud()
+        media_list = lc.get_list(media_id=media_id)
+        if media_list:
+            filename = lc.get_media(media_id).get("media_name")
+            self.set_header("Content-Type", "application/octet-stream")
+            self.set_header("Content-Disposition",
+                            "attachment; filename=" + filename + ".lrc")
+
+            self.write_content(media_list)
+            self.finish()
+
+        else:
+            self.write("not exist")
+
+    def fmt_time(self,seconds):
+        seconds = round(seconds, 2)
+        # t_start = datetime.datetime(1970, 1, 1)
+        # t_delta = datetime.timedelta(seconds=seconds)
+        # t_end = t_start + t_delta
+        # time_tuple = (t_end.hour - t_start.hour,
+        #               t_end.minute - t_start.minute,
+        #               t_end.second - t_start.second,
+        #               t_end.microsecond - t_start.microsecond)
+        minute, second = divmod(seconds,60)
+        return "[%s:%s]" % (str(int(minute)), str(second))
+
+    def fmt_content(self, media):
+        content_list = media.get(self.content_key)
+        content = content_list[0] if content_list else ""
+        content = re.sub(u"[,，。\.?？!！]"," ",content)
+        return content
+
+    def write_content(self, media_list):
+        for (index, media) in enumerate(media_list, 1):
+            self.write(self.fmt_time(media.get("start_at")))
+            self.write(self.fmt_content(media))
+            self.write("\n")
+
+
 def make_app(use_autoreload):
     return tornado.web.Application([
         (r"/test", TestHandler),
         (r"/transcribe", TranscribeHandler),
-        (r"/medium/(.*)/srt", SrtHandler)
+        (r"/medium/(.*)/srt", SrtHandler),
+        (r"/medium/(.*)/lrc", LrcHandler),
     ], autoreload=use_autoreload)
 
 
