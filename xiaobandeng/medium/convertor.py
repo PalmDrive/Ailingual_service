3f6f14ff2bb8d97ee43da95cd05d5827650679d0
jml <ljm@a.cc>
2016-10-17 10:43:50 +0800

    fix disk full and update readme about supervisor.

--- xiaobandeng/medium/convertor.py
+++ xiaobandeng/medium/convertor.py
@@ -11,7 +11,7 @@ def convert_to_wav(ext, filename):
         if ext.endswith(f):
             supported = True
     if supported:
-        tmp_file = tempfile.NamedTemporaryFile().name + ".wav"
+        tmp_file = tempfile.NamedTemporaryFile(suffix = ".wav").name
         print subprocess.call(
             'ffmpeg -i %s -ac 1 -ar 16000 %s' % (filename, tmp_file),
             shell=True)
