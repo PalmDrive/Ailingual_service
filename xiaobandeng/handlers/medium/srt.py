de63ce0d8aeae2bce5666bd60ea9cc39d9b88727
jml <ljm@a.cc>
2016-10-24 20:45:09 +0800

    fix lrc and srt by media service_provider

--- xiaobandeng/handlers/medium/srt.py
+++ xiaobandeng/handlers/medium/srt.py
@@ -11,15 +11,18 @@ from ..base import BaseHandler
 
 class SrtHandler(BaseHandler):
     def get(self, media_id):
-        source = self.get_argument("service_source", 0)
-        platform = self.get_argument("plat", 'unix')
+        # source = self.get_argument("service_source", 0)
+        platform = self.get_argument("plat", 'win')
 
-        lc_content_keys = ["content_baidu", "content_google"]
-        content_key = lc_content_keys[int(source)]
+        # lc_content_keys = ["content_baidu", "content_google"]
+        # content_key = lc_content_keys[int(source)]
 
         lc = lean_cloud.LeanCloud()
-        media_list = lc.get_list(media_id=media_id)
-
+        fragment_list = lc.get_list(media_id=media_id)
+        media = lc.get_media(media_id)
+        content_keys = ["content_" + i for i in media.get("service_providers")]
+        if not content_keys:
+            content_keys = ["content_baidu"]
 
         def convert_time(seconds):
             seconds = round(seconds, 3)
@@ -32,10 +35,11 @@ class SrtHandler(BaseHandler):
                           t_end.microsecond - t_start.microsecond)
 
             # print ":".join(["%02d"%i for i in time_tuple[:-1]]) + "," + \
-            #        "%d" % (time_tuple[-1] / 1000)
+            # "%d" % (time_tuple[-1] / 1000)
 
-            return ":".join(["%02d"%i for i in time_tuple[:-1]]) + "," + \
+            return ":".join(["%02d" % i for i in time_tuple[:-1]]) + "," + \
                    "%03d" % (time_tuple[-1] / 1000)
+
         if platform == "win":
             sep = "\r\n"
             encoding = "gbk"
@@ -43,26 +47,28 @@ class SrtHandler(BaseHandler):
             sep = "\n"
             encoding = "utf8"
 
-        if media_list:
-            filename = lc.get_media(media_id).get("media_name")
+        if fragment_list:
+            filename = media.get("media_name")
             self.set_header("Content-Type", "application/octet-stream")
             self.set_header("Content-Disposition",
                             "attachment; filename=" + filename + ".srt")
-            for (index, media) in enumerate(media_list, 1):
-                self.write(str(media.get("fragment_order") + 1))
+            for (index, fragment) in enumerate(fragment_list, 1):
+                self.write(str(fragment.get("fragment_order") + 1))
                 self.write(sep)
 
                 self.write(
-                    convert_time(media.get("start_at")) + " --> " +
-                    convert_time(media.get("end_at")))
+                    convert_time(fragment.get("start_at")) + " --> " +
+                    convert_time(fragment.get("end_at")))
                 self.write(sep)
 
-                content_list = media.get(content_key)
+                for content_key in content_keys:
+                    content_list = fragment.get(content_key)
 
-                content = content_list[0] if content_list else ""
-                content = re.sub(u"[,，。\.?？!！]", " ", content)
-                self.write(content.encode(encoding))
-                self.write(sep)
+                    content = content_list[0] if content_list else ""
+                    content = re.sub(u"[,，。\.?？!！]", " ", content)
+                    self.write(content.encode(encoding))
+                    self.write(sep)
+
                 self.write(sep)
             self.finish()
 
