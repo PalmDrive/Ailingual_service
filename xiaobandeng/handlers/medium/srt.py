8921abf4e37689d6e6431b7b08a4ef413f2bc9c9
jml <ljm@a.cc>
2016-10-29 16:19:35 +0800

    refactor code.

--- xiaobandeng/handlers/medium/srt.py
+++ xiaobandeng/handlers/medium/srt.py
@@ -20,11 +20,8 @@ class SrtHandler(BaseHandler):
         lc = lean_cloud.LeanCloud()
         fragment_list = lc.get_list(media_id=media_id)
         media = lc.get_media(media_id)
-        provider_list = media.get("service_providers") or []
-
+        provider_list = media.get("service_providers") or ["baidu"]
         content_keys = ["content_" + i for i in provider_list]
-        if not content_keys:
-            content_keys = ["content_baidu"]
 
         def convert_time(seconds):
             seconds = round(seconds, 3)
