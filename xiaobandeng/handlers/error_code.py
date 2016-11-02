07051ec87747c65f32b12c9ee4a1a0b4de28605e
Yong <yonglin88@gmail.com>
2016-11-02 13:41:39 -0700

    support all media types

--- xiaobandeng/handlers/error_code.py
+++ xiaobandeng/handlers/error_code.py
@@ -1,8 +1,13 @@
 # coding:utf-8
 ECODE = type("ErrorCode", (), {})()
 
+# media
+ECODE.ERR_MEDIA_DOWNLOAD_FAILURE = (100003, "Media download error or timeout. Check your media address.")
+ECODE.ERR_MEDIA_UNSUPPORTED_FORMAT = (100004, "Media's format is unsupported. Check your media format.")
+
 # user
 # 200000-200100
 ECODE.ERR_USER_HAVE_APP_INFO = (200001, "app_info already exists")
 ECODE.ERR_USER_NO_THAT_USER = (200002, "user not found")
 ECODE.ERR_USER_NO_THAT_APP_INFO = (200003, "app_key and app_id mismatch")
+
--- xiaobandeng/medium/convertor.py
+++ xiaobandeng/medium/convertor.py
@@ -5,16 +5,8 @@ import tempfile
 
 
 def convert_to_wav(ext, filename):
-    supported_formats = ['mp4', 'mp3', 'm4a']
-    supported = False
-    for f in supported_formats:
-        if ext.endswith(f):
-            supported = True
-    if supported:
-        tmp_file = tempfile.NamedTemporaryFile(suffix = ".wav").name
-        print subprocess.call(
-            'ffmpeg -i %s -ac 1 -ar 16000 %s' % (filename, tmp_file),
-            shell=True)
-        return tmp_file
-    else:
-        return filename
+    tmp_file = tempfile.NamedTemporaryFile(suffix = ".wav").name
+    print subprocess.call(
+        'ffmpeg -i %s -ac 1 -ar 16000 %s' % (filename, tmp_file),
+        shell=True)
+    return tmp_file
