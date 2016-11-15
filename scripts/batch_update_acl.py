b26bff876ee9e8736aa2ad2b4b1fb0e9c9ccf190
jml <ljm@a.cc>
2016-11-15 15:06:13 +0800

    add script for update acl of lean cloud.notice set default acl

--- scripts/batch_update_acl.py
+++ scripts/batch_update_acl.py
@@ -1,9 +1,12 @@
+# coding:utf8
 from __future__ import absolute_import
+import os, sys
+
+sys.path.append("/Users/lijim001/my_code/palmdrive/pipeline_service")
 
 from xiaobandeng.config import load_config
 from xiaobandeng.lean_cloud import init
 from xiaobandeng.config import CONFIG
-from xiaobandeng.lean_cloud import lean_cloud
 import leancloud
 
 env = 'product'
@@ -14,4 +17,21 @@ CLASS_NAME = 'EditorTask'
 
 lean_cloud_class = leancloud.Object.extend(CLASS_NAME)
 
-lean_cloud_class
+query = lean_cloud_class.query
+query.add_ascending("createdAt")
+query.limit(1000)
+
+obj_list = query.find()
+print 'total:',len(obj_list)
+
+
+# obj = obj_list[0]
+
+for obj in obj_list:
+    acl = leancloud.ACL()
+    acl.set_public_read_access(True)
+    acl.set_public_write_access(True)
+
+    obj.set_acl(acl)
+    obj.save()
+    print obj.id
