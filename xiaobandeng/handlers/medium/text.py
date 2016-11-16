ce0f3c70fce26a9e26800559995ce2baa545710d
jml <ljm@a.cc>
2016-11-16 17:07:50 +0800

    refactor download media

--- xiaobandeng/handlers/medium/text.py
+++ xiaobandeng/handlers/medium/text.py
@@ -1,42 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
-
-from xiaobandeng.lean_cloud import lean_cloud
-
-from ..base import BaseHandler
+from .download_base import DownloadHandler
 
 
-class TextHandler(BaseHandler):
-    def get(self, media_id):
-        service_provider = self.get_argument("service_provider", "baidu")
+class TextHandler(DownloadHandler):
+    def handle(self, media, fragment_list, content_keys, sep, encoding):
 
-        lc = lean_cloud.LeanCloud()
-        media = lc.get_media(media_id)
-        transcript_sets_map = media.get("transcript_sets")
-        set_type_order = ["timestamp", "ut", "machine"]
-
-        set_type_to_download = "machine"
-        for set_type in set_type_order:
-            if transcript_sets_map.get(set_type):
-                set_type_to_download = set_type
-                break
-        print  'set type :%s' % set_type_to_download
-
-        all_transcript = lc.get_list(media_id, set_type_to_download)
-        content_key = "content_" + service_provider
-
-        if all_transcript:
-
+        if fragment_list:
+            content_key = content_keys[0]
             content = [transcript.get(content_key)[0] if transcript.get(
-                content_key) else "" for transcript in
-                       all_transcript]
+                content_key) else "" for transcript in fragment_list]
 
             filename = media.get("media_name")
             self.set_header("Content-Type", "application/octet-stream")
             self.set_header("Content-Disposition",
                             "attachment; filename=" + filename + ".txt")
-            self.write(''.join(content))
+            self.write((''.join(content)).encode(encoding))
             self.finish()
         else:
             self.write("not exist")
